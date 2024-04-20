6fd4832d763e15e0690877abf6d14b04c835932e
MadalinaZanficu <zanficumadalina@gmail.com>
2024-04-20 19:56:13 +0300

    Added user DB type filed. The user can be of type: 'seller' or 'buyer'.

--- auth-server/db_entities.py
+++ auth-server/db_entities.py
@@ -4,3 +4,4 @@ from mongoengine import Document, StringField
 class Users(Document):
     username = StringField(required=True, unique=True)
     password = StringField(required=True)
+    type = StringField(required=True)
