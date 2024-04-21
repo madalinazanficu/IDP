c77f6c3ac2378c221f81792804095d4691ab1c45
mihneadinik <mihneadinica@gmail.com>
2024-04-21 16:28:03 +0300

    implement logic for publishing products

--- io-service/db_entities.py
+++ io-service/db_entities.py
@@ -1,5 +1,5 @@
 import mongoengine
-from mongoengine import Document, StringField, SequenceField, FloatField, CASCADE, ReferenceField
+from mongoengine import Document, StringField, SequenceField, FloatField, CASCADE, ReferenceField, IntField
 
 
 class Categories(Document):
@@ -8,9 +8,10 @@ class Categories(Document):
 
 class Products(Document):
     _id = SequenceField(primary_key=True)
-    product = StringField(required=True, unique=True)
+    product = StringField(required=True)
     price = FloatField(required=True)
-    quantity = FloatField(required=True)
+    quantity = IntField(required=True)
     description = StringField(required=True)
     category = ReferenceField(Categories, reverse_delete_rule=CASCADE)
+    username = StringField(required=True)
 
